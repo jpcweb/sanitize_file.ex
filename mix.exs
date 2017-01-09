30b2cdc849724f122af447d59947e569c0dd4a76
Jérémie Payet <payet.jeremie@gmail.com>
2017-01-09 13:14:58 -1000

    mix file changed

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule SanitizeFile.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
